03cc59580e3f672b5c9453172c461df7dfd3f512
Sanom4 <alexander@gonzalez.ge>
2023-08-25 13:49:37 +0300

    fix urls

--- themes/infinity/index.php
+++ themes/infinity/index.php
@@ -13,7 +13,7 @@ get_header(); ?>
 
                     foreach ($Categories as $category) :
                     ?>
-                        <li><a href='<?php home_url('/?category=') .  $category; ?>'><?php echo ucfirst(str_replace('-', ' ', $category)); ?> </a></li>
+                        <li><a href='<?php echo home_url('/?category=') . $category; ?>'><?php echo ucfirst(str_replace('-', ' ', $category)); ?> </a></li>
 
                     <?php
                     endforeach;
@@ -32,10 +32,10 @@ get_header(); ?>
                 ?>
 
                         <div class="col-md-6">
-                            <div class="product text-center"> <a href='<?php home_url('/product/') . $product['id']; ?>'>
+                            <div class="product text-center"> <a href='<?php echo home_url('/product/') . $product['id']; ?>'>
                                     <img class="productImage" src="<?php echo $product['thumbnail']; ?>" width="250"></a>
                                 <div class="about text-left px-3">
-                                    <h4><a href='/product/<?php home_url('/product/') . $product['id']; ?>'><?php echo $product['title']; ?></a>
+                                    <h4><a href='/product/<?php echo home_url('/product/') . $product['id']; ?>'><?php echo $product['title']; ?></a>
                                         <h3><?php echo $product['price']; ?>$</h3>
                                 </div>
                                 <a href="#" class="btn btn-primary cd-add-to-cart js-cd-add-to-cart" data-price="<?php echo $product['price']; ?>" data-id="<?php echo $product['id']; ?>">Add to Cart</a>
