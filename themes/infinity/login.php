0be4c6249a1f100d431cb036759300058f80f9f9
Sanom4 <alexander@gonzalez.ge>
2023-08-27 10:09:28 +0300

    push login and reguistration page

--- themes/infinity/login.php
+++ themes/infinity/login.php
@@ -20,7 +20,16 @@ get_header();
             </div>
         </div>
         <div class="col-md-8">
-
+            <div class="mb-3">
+                <div class="form-check form-check-inline">
+                    <input class="form-check-input" type="radio" name="userAction" id="registerRadio" value="register" checked>
+                    <label class="form-check-label" for="registerRadio">Register</label>
+                </div>
+                <div class="form-check form-check-inline">
+                    <input class="form-check-input" type="radio" name="userAction" id="loginRadio" value="login">
+                    <label class="form-check-label" for="loginRadio">Login</label>
+                </div>
+            </div>
 
         </div>
     </div>
--- themes/infinity/register.php
+++ themes/infinity/register.php
@@ -14,13 +14,27 @@ get_header();
             <div class="sideBlock bg-white p-3">
                 <ul class="list-unstyled">
                     <li><a href='<?php echo esc_url(home_url('/login')); ?>'>Login</a></li>
-                    <li class="active"><a href='<?php echo esc_url(home_url('/register')); ?>'>Login</a></li>
+                    <li class="active"><a href='<?php echo esc_url(home_url('/register')); ?>'>Register</a></li>
 
                 </ul>
             </div>
         </div>
         <div class="col-md-8">
-
+            <div class="registrationSection mb-3">
+                <h3 class="mb-3">Registration</h3>
+                <div class="mb-3">
+                    <label for="username" class="form-label">Username</label>
+                    <input type="text" class="form-control" id="username" name="username">
+                </div>
+                <div class="mb-3">
+                    <label for="password" class="form-label">Password</label>
+                    <input type="password" class="form-control" id="password" name="password">
+                </div>
+                <div class="mb-3">
+                    <label for="email" class="form-label">Email</label>
+                    <input type="text" class="form-control" id="email" name="email">
+                </div>
+            </div>
 
         </div>
     </div>
